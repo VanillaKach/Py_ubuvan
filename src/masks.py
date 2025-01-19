a05e9640ef985c10d44399ba0350974851b9d9f3
Ivan <4d4m5@mail.ru>
2025-01-19 09:30:47 +0300

    Исправлены функции в модулях masks.py и widget.py. Исправлены тесты для этих функций.

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,13 @@
 def get_mask_card_number(card_number: str) -> str:
     """Маскировка номера банковской карты"""
-    mask_card_number: str = card_number.replace(card_number[6:12], "** **** ")
-    mask_card_number = mask_card_number[:4] + " " + mask_card_number[4:]
-    return mask_card_number
+    if len(card_number) == 16:
+        mask_card_number: str = card_number.replace(card_number[6:12], "** **** ")
+        mask_card_number = mask_card_number[:4] + " " + mask_card_number[4:]
+        return mask_card_number
+    elif not card_number.isdigit():
+        return "Номер карты не указан!"
+    elif len(card_number) != 16:
+        return "Номер карты указан не верно!"
 
 
 print(get_mask_card_number("7000792289606361"))
@@ -10,8 +15,12 @@ print(get_mask_card_number("7000792289606361"))
 
 def get_mask_account(bank_account_number: str) -> str:
     """Маскировка номера банковского счёта"""
-    mask_account: str = bank_account_number.replace(bank_account_number[:-4], "**")
-    return mask_account
-
+    if len(bank_account_number) == 20:
+        mask_account: str = bank_account_number.replace(bank_account_number[:-4], "**")
+        return mask_account
+    elif not bank_account_number.isdigit():
+        return "Номер счёта не указан!"
+    elif len(bank_account_number) != 20:
+        return "Номер счёта указан не верно!"
 
 print(get_mask_account("73654108430135874305"))
--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,5 @@
 def mask_account_card(number_card_or_score_account: str) -> str:
     """Функция для маскировки номера банковской карты и банковского счёта"""
-    number_card_or_score_account.split(",")
     if len(number_card_or_score_account.split(" ")[-1]) == 16:
         for number in number_card_or_score_account.split():  # маскировка номера карты
             if number.isdigit():
@@ -10,7 +9,7 @@ def mask_account_card(number_card_or_score_account: str) -> str:
                 pass
         mask_card = number_card_or_score_account.replace(number_card_or_score_account[-16:], result_number)
         return mask_card
-    else:
+    elif len(number_card_or_score_account.split(" ")[-1]) == 20:
         mask_score_account = number_card_or_score_account.replace(number_card_or_score_account[:-4], "**")
 
         for score in number_card_or_score_account.split():  # маскировка номера счёта
@@ -20,20 +19,23 @@ def mask_account_card(number_card_or_score_account: str) -> str:
                 pass
         mask_account = number_card_or_score_account.replace(number_card_or_score_account[-20:], mask_score_account)
         return mask_account
+    else:
+        return "Номер карты или счёта указан не верно!"
 
-
-print(mask_account_card("Visa Platinum 7000792289606361"))
+print(mask_account_card("Счет 73654108430135874305"))
 # Visa Platinum 7000792289606361
 # Счет 73654108430135874305
 
 
 def get_date(old_date: str) -> str:
     """Функция для переформатирования даты в формате 'дд.мм.гггг'"""
-    year = old_date[0:4]
-    month = old_date[5:7]
-    day = old_date[8:10]
-    new_date = f"{day}.{month}.{year}"
-    return new_date
-
+    if len(old_date) == 26:
+        year = old_date[0:4]
+        month = old_date[5:7]
+        day = old_date[8:10]
+        new_date = f"{day}.{month}.{year}"
+        return new_date
+    else:
+        return "Дата указана не верно!"
 
 print(get_date("2024-03-11T02:26:18.671407"))
