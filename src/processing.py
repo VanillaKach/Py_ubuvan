2eb579350fe13cf1326b40c4c4f4e10ae64bd2e9
Ivan <4d4m5@mail.ru>
2025-01-19 11:34:23 +0300

    Готовы Тесты к модулям в пакете src/. Добавлен файл .coverage с отчётом о выполненных тестах.

--- src/processing.py
+++ src/processing.py
@@ -1,16 +1,12 @@
 from typing import Any, Dict, List, Optional
 
 
-def filter_by_state(transactions: list, state: str = "EXECUTED") -> List[Dict]:
-    """
-    Фильтрует список словарей по значению ключа 'state'.
-    :param transactions: Список словарей с транзакциями.
-    :param state: Значение для фильтрации по ключу 'state' (по умолчанию 'EXECUTED').
-    :return: Отфильтрованный список словарей.
-    """
-    return [transact for transact in transactions if transact.get("state") == state]
+def filter_by_state(transactions: list, state_default: str = "EXECUTED") -> List[Dict]:
+    """Фильтрует список словарей по значению ключа 'state'"""
 
+    return [transact for transact in transactions if transact.get("state") == state_default]
 
+# Пример входных данных для проверки функции
 transactions = [
     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
@@ -18,17 +14,10 @@ transactions = [
 ]
 
 print(filter_by_state(transactions))  # Фильтр по умолчанию 'EXECUTED'
-print(filter_by_state(transactions, "CANCELED"))  # Фильтр по 'CANCELED'
 
 
 def sort_by_date(data: List[Dict[str, Any]], descending: Optional[bool] = True) -> List[Dict[str, str]]:
-    """
-    Сортирует список словарей по дате.
-
-    :param data: Список словарей, содержащих поле 'date'.
-    :param descending: Параметр, задающий порядок сортировки (по умолчанию True - убывание).
-    :return: Новый список, отсортированный по дате.
-    """
+    """Сортирует список словарей по дате"""
     return sorted(data, key=lambda x: x["date"], reverse=bool(descending))
 
 
@@ -39,7 +28,6 @@ input_data = [
     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
-
 # Тестирование функции
 sorted_data = sort_by_date(input_data)
 print(sorted_data)
