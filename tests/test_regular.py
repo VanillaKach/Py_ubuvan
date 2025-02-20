33b4f88f6e4fee40c111f504b145a78c48c5df06
Ivan <4d4m5@mail.ru>
2025-02-20 19:13:39 +0300

    Исправлены функциональные ошибки в модуле regular.py и модуль с тестами к нему. Написана функция main() для объединения работы с модулями.

--- tests/test_regular.py
+++ tests/test_regular.py
@@ -1,9 +1,12 @@
 import pytest
 from src.regular import count_operations_by_type, search_operations
 from collections import Counter
+from typing import List, Dict
+
+# Фикстуры для тестов
 
 @pytest.fixture
-def sample_transactions_for_count():
+def sample_transactions_for_count() -> List[Dict]:
     """Фикстура для списка транзакций для подсчета"""
     return [
         {"id": 441945886, "state": "EXECUTED", "date": "2019-08-26T10:50:58.294041", "description": "Перевод организации"},
@@ -13,32 +16,34 @@ def sample_transactions_for_count():
     ]
 
 @pytest.fixture
-def sample_categories():
-    """Фикстура для словаря категорий"""
-    return {
-        "Перевод организации": "Организационные переводы",
-        "Открытие вклада": "Вклады",
-        "Перевод со счета на счет": "Межсчетовые переводы",
-    }
+def sample_categories() -> List[str]:
+    """Фикстура для списка категорий"""
+    return [
+        "Перевод организации",
+        "Открытие вклада",
+        "Перевод со счета на счет",
+    ]
+
+# Тесты для функции count_operations_by_type()
 
 def test_count_operations_by_type(sample_transactions_for_count, sample_categories):
     expected_result = Counter({
-        "Организационные переводы": 2,
-        "Вклады": 1,
-        "Межсчетовые переводы": 1,
+        "Перевод организации": 2,
+        "Открытие вклада": 1,
+        "Перевод со счета на счет": 1,
     })
     result = count_operations_by_type(sample_transactions_for_count, sample_categories)
     assert result == dict(expected_result)
 
-
+# Тесты для функции search_operations()
 
 @pytest.fixture
-def sample_transactions():
+def sample_transactions() -> List[Dict]:
     """Фикстура для списка транзакций"""
     return [
         {"id": 441945886, "state": "EXECUTED", "date": "2019-08-26T10:50:58.294041", "description": "Перевод организации"},
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364", "description": "Перевод организации"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572", "description": "Перевод организации"},
+        {"id": 939719570, "state": "EXECUTED", "date":  "2018-06-30T02:08:58.425572", "description": "Перевод организации"},
         {"id": 587085106, "state": "EXECUTED", "date": "2018-03-23T10:45:06.972075", "description": "Открытие вклада"},
         {"id": 142264268, "state": "EXECUTED", "date": "2019-04-04T23:20:05.206878", "description": "Перевод со счета на счет"},
     ]
